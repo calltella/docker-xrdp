b1a38648bc1a9c79816d8a190f0b3cee8a3d65cb
calltella <yuichi.asa@gmail.com>
2022-09-13 15:08:42 +0900

    add firefox

--- entrypoint-xlde.sh
+++ entrypoint-xlde.sh
@@ -4,8 +4,11 @@ USER_NAME=$(id -u -n)
 echo $USER_NAME
 if test -f "/tmp/.X1-lock"; then
 	  rm /tmp/.X1-lock
-  	  rm /tmp/.X11-unix/X1
+  	rm /tmp/.X11-unix/X1
 fi
+
+# ls dir color 変更
+echo "export LS_COLORS='di=01;36'" >> ~/.bashrc
 
 /usr/bin/vncserver :1 -name ${VNC_DISPLAYNAME} -geometry ${VNC_RESOLUTION} -depth 24
 tail -f /home/$USER_NAME/.vnc/*:1.log
